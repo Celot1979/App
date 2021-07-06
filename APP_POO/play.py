ed952d4b43c778f7bbd6465c019837ff926287bd
Celot1979 <zatak631979@gmail.com>
2021-07-06 20:42:42 +0200

    Creación de la raiz

---
+++ APP_POO/play.py
@@ -0,0 +1,14 @@
+from tkinter import Tk, messagebox
+raiz = Tk()
+
+#Quitado
+class Protectora:
+    def __init__(self, ventana):
+        self.ventana = ventana
+        self.ventana.title("Aplicación en POO ")
+        
+        
+
+
+Proyecto = Protectora(raiz)
+raiz.mainloop()
